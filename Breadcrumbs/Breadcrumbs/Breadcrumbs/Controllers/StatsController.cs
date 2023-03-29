7e95316e6ee34cf2a0390ba7e7fa3e1566df9d3b
Blü <91242410+BluWhiteBear@users.noreply.github.com>
2023-03-29 11:08:13 -0400

    updated all the things

--- Breadcrumbs/Breadcrumbs/Breadcrumbs/Controllers/StatsController.cs
+++ Breadcrumbs/Breadcrumbs/Breadcrumbs/Controllers/StatsController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Breadcrumbs.Models;
 
 namespace Breadcrumbs.Controllers
 {
     public class StatsController : Controller
     {
-        public IActionResult Index()
+        private readonly breadcrumbsContext _context;
+
+        public StatsController(breadcrumbsContext context)
         {
-            return View();
+            _context = context;
+        }
+        
+        // return the count of all locations
+        public async Task<IActionResult> Index()
+        {
+            var stats = new Stats();
+            // Overview
+            stats.LocationCount = await _context.Locations.CountAsync();
+            stats.DestinationCount = await _context.Destinations.CountAsync();
+            stats.UserCount = await _context.AspNetUsers.CountAsync();
+
+            // Returns the location with the highest number of attached destinations
+            stats.MostDestinations = await _context.Locations
+                .OrderByDescending(l => l.Destinations.Count)
+                .Select(l => l.LocationName)
+                .FirstOrDefaultAsync();
+
+            return View(stats);
         }
     }
 }
---
+++ Breadcrumbs/Breadcrumbs/Breadcrumbs/Models/Stats.cs
@@ -0,0 +1,14 @@
+﻿namespace Breadcrumbs.Models
+{
+    public class Stats
+    {
+        // Count of items in AspNetUsers
+        public int UserCount { get; set; }
+        // Count of items in Locations
+        public int LocationCount { get; set; }
+        // Count of items in Destinations
+        public int DestinationCount { get; set; }
+        // Name of location with most destinations
+        public string MostDestinations { get; set; }
+    }
+}
